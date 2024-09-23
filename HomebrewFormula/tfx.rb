70ff3148305b2da1327879683023cc0cf827e539
straubt1 <tstraub@hashicorp.com>
2024-09-23 11:41:11 -0500

    Updates

--- HomebrewFormula/tfx.rb
+++ HomebrewFormula/tfx.rb
@@ -1,7 +1,7 @@
 class Tfx < Formula
   desc "tfx cli --  TFx is a standalone CLI for Terraform Cloud and Terraform Enterprise."
   homepage "https://github.com/straubt1/tfx"
-  url "git@github.com:straubt1/tfx.git", :using => :git, :tag => "0.1.3"
+  url "git@github.com:straubt1/tfx.git", :using => :git, :tag => "v0.1.3"
   head "git@github.com:straubt1/tfx.git", :using => :git, :branch => "main"
   version '0.1.3'
 
@@ -17,7 +17,7 @@ class Tfx < Formula
     go_ldflags = "-w -s"
     go_ldflags << " -X 'github.com/straubt1/tfx/version.Version=#{version}'"
     go_ldflags << " -X 'github.com/straubt1/tfx/version.BuiltBy=HomeBrew'"
-    system "go", "build", "-trimpath", "-ldflags", go_ldflags, "-o", bin/"tfm"
+    system "go", "build", "-trimpath", "-ldflags", go_ldflags, "-o", bin/"tfx"
   end
   def caveats
     <<~EOS
