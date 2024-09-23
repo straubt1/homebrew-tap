7870d1545bb50276855ad374140d5686ab1af3e5
straubt1 <tstraub@hashicorp.com>
2024-09-23 13:41:46 -0500

    Update to Formula Folder

---
+++ Formula/tfx.rb
@@ -0,0 +1,29 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Tfx < Formula
+  desc "TFx is a standalone CLI for Terraform Cloud and Terraform Enterprise."
+  homepage ""
+  url "https://github.com/straubt1/tfx/releases/download/v0.1.3/tfx_Darwin_arm64.tar.gz"
+  sha256 "02d89199c3336beb0c9525cda5334ca52fe8cb02bba0ca26923640996978c998"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test tfx`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    system "false"
+  end
+end
