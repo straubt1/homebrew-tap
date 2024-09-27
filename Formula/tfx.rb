bcc141296c5228afa511788ee78e9a33b773f56e
straubt1 <tstraub@hashicorp.com>
2024-09-27 12:04:43 -0500

    Added linux

--- Formula/tfx.rb
+++ Formula/tfx.rb
@@ -26,6 +26,29 @@ class Tfx < Formula
     end
   end
 
+  on_linux do
+    on_intel do
+      if Hardware::CPU.is_64_bit?
+        url "https://github.com/straubt1/tfx/releases/download/v0.1.3/tfx_Linux_x86_64.tar.gz"
+        sha256 "5d2dc26053e7e981100877179b6ddc2c2fbd6ad313afceb94e3fdd259f559ac8"
+
+        def install
+          bin.install "tfx"
+        end
+      end
+    end
+    on_arm do
+      if Hardware::CPU.is_64_bit?
+        url "https://github.com/straubt1/tfx/releases/download/v0.1.3/tfx_Linux_arm64.tar.gz"
+        sha256 "2a7caeb0195b1350415457ec58cee3c23fbcaaa3d64c213da4f90ca3b01fec51"
+
+        def install
+          bin.install "tfx"
+        end
+      end
+    end
+  end
+
   test do
     system "tfx -v"
   end
