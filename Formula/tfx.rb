facc0405124236977a4e667d251577f6e94f4952
Tom Straub <tstraub@hashicorp.com>
2024-10-29 11:57:37 -0500

    Update tfx.rb

--- Formula/tfx.rb
+++ Formula/tfx.rb
@@ -5,20 +5,20 @@
 class Tfx < Formula
   desc "tfx cli --  TFx is a standalone CLI for Terraform Cloud and Terraform Enterprise."
   homepage "https://github.com/straubt1/tfx"
-  version "0.1.3"
+  version "0.1.4"
 
   on_macos do
     on_intel do
-      url "https://github.com/straubt1/tfx/releases/download/v0.1.3/tfx_Darwin_x86_64.tar.gz"
-      sha256 "5d2dc26053e7e981100877179b6ddc2c2fbd6ad313afceb94e3fdd259f559ac8"
+      url "https://github.com/straubt1/tfx/releases/download/v0.1.4/tfx_Darwin_x86_64.tar.gz"
+      sha256 "fa1afd5cac81862f939f888eaeacda9fa25a5f1424b587e04e844618acc4165c"
 
       def install
         bin.install "tfx"
       end
     end
     on_arm do
-      url "https://github.com/straubt1/tfx/releases/download/v0.1.3/tfx_Darwin_arm64.tar.gz"
-      sha256 "02d89199c3336beb0c9525cda5334ca52fe8cb02bba0ca26923640996978c998"
+      url "https://github.com/straubt1/tfx/releases/download/v0.1.4/tfx_Darwin_arm64.tar.gz"
+      sha256 "b3c532fef5d60dc5ac968e44fd3818e24e2132de833e81ab3bbb43c32aedc588"
 
       def install
         bin.install "tfx"
@@ -29,8 +29,8 @@ class Tfx < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/straubt1/tfx/releases/download/v0.1.3/tfx_Linux_x86_64.tar.gz"
-        sha256 "62bec3183e2f0e6caadc01c1c5c164ff71dba27f883a1c5a6f95bdd09139f048"
+        url "https://github.com/straubt1/tfx/releases/download/v0.1.4/tfx_Linux_x86_64.tar.gz"
+        sha256 "71eac1f093865791261a0a1298c5c8b38bf2c165f879f5af024f9ad9cda0fbb2"
 
         def install
           bin.install "tfx"
@@ -39,8 +39,8 @@ class Tfx < Formula
     end
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/straubt1/tfx/releases/download/v0.1.3/tfx_Linux_arm64.tar.gz"
-        sha256 "2a7caeb0195b1350415457ec58cee3c23fbcaaa3d64c213da4f90ca3b01fec51"
+        url "https://github.com/straubt1/tfx/releases/download/v0.1.4/tfx_Linux_arm64.tar.gz"
+        sha256 "1c44168892c6d50c1b65f28ac707b86049a382ff7ab515bce21c4502197f2b5d"
 
         def install
           bin.install "tfx"
